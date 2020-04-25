f9e9f4ee91407e87558a44f31bb10fa5dd24b754
yohanym95 <malshikay@gmail.com>
2020-04-25 17:22:25 +0530

    Create home main page UI widgets

--- lib/Components/components.dart
+++ lib/Components/components.dart
@@ -3,7 +3,7 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:readhubnew/Components/ovalrightborderclipper.dart';
 
 String url =
-    'https://firebasestorage.googleapis.com/v0/b/blogapp-463a6.appspot.com/o/2.png?alt=media&token=587147af-8d63-433b-a2d1-04221ba8fd90';
+    'https://firebasestorage.googleapis.com/v0/b/blogapp-463a6.appspot.com/o/readhublogo.png?alt=media&token=f20f89a5-77fe-432a-84db-a93f5f821411';
 
 buildDrawer() {
   return ClipPath(
@@ -75,5 +75,93 @@ Widget buildRow(IconData icon, String title) {
         )
       ],
     ),
+  );
+}
+
+Widget getCardComponent(height) {
+  return Card(
+      color: Colors.white,
+      elevation: 3.0,
+      child: Padding(
+        padding: const EdgeInsets.all(4.0),
+        child: Column(
+          children: <Widget>[
+            Image(
+              image: AssetImage('assets/images/angular.png'),
+              height: height / 9,
+              width: height / 9,
+            ),
+            Container(
+              alignment: Alignment.center,
+              child: Text('Angular'),
+            ),
+          ],
+        ),
+      ));
+}
+
+Widget getTopicChip(topic) {
+  return Padding(
+    padding: const EdgeInsets.only(left: 4.0),
+    child: Chip(
+      elevation: 3,
+      label: Text(
+        topic,
+        style: TextStyle(fontWeight: FontWeight.bold, color: Colors.grey[350]),
+      ),
+      backgroundColor: Colors.yellow[800],
+    ),
+  );
+}
+
+Widget getRecentCardComponent(height) {
+  return Container(
+    width: height/3,
+    child: Card(
+        color: Colors.white,
+        elevation: 3.0,
+        child: Padding(
+          padding: const EdgeInsets.only(left:4.0,right: 4.0,top: 4.0),
+          child: Column(
+            children: <Widget>[
+              Row(
+                children: <Widget>[
+                  Container(
+                    child: CircleAvatar(
+                      child: Icon(Icons.person),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 8,
+                  ),
+                  Text(
+                    'Yohan Malshika',
+                    style: TextStyle(fontSize: 17, fontWeight: FontWeight.bold),
+                  )
+                ],
+              ),
+              Image(
+                image: AssetImage('assets/images/recent.png'),
+                height: height / 6,
+                width: height / 4,
+              ),
+              Container(
+                alignment: Alignment.topLeft,
+                child: Text(
+                  'ReadHub is moving to flutter',
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.only(top: 4.0),
+               alignment: Alignment.bottomLeft,
+                child: Text(
+                  'Date-2020.09.30',
+                  style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+                ),
+              ),
+            ],
+          ),
+        )),
   );
 }
--- lib/main.dart
+++ lib/main.dart
@@ -24,39 +24,181 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  var data = ['Sinhala', 'English'];
+  bool isSelected = false;
   @override
   Widget build(BuildContext context) {
+    double height = MediaQuery.of(context).size.height;
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            Image(
-              image: NetworkImage(url),
-              width: 50,
-              height: 50,
-            ),
-            Text(
-              'ReadHub',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                  letterSpacing: 3, fontWeight: FontWeight.bold, fontSize: 20),
-            )
-          ],
+        appBar: AppBar(
+          centerTitle: true,
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Image(
+                image: NetworkImage(url),
+                width: 50,
+                height: 50,
+              ),
+              Text(
+                'ReadHub',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    letterSpacing: 3,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20),
+              )
+            ],
+          ),
+          backgroundColor: Colors.blue,
+          flexibleSpace: Container(
+            decoration: BoxDecoration(
+                gradient: LinearGradient(
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomRight,
+                    colors: <Color>[Colors.blue[200], Colors.blue])),
+          ),
         ),
-        backgroundColor: Colors.blue,
-        flexibleSpace: Container(
-          decoration: BoxDecoration(
-              gradient: LinearGradient(
-                  begin: Alignment.topLeft,
-                  end: Alignment.bottomRight,
-                  colors: <Color>[Colors.blue[200], Colors.blue])),
-        ),
+        drawer: buildDrawer(),
+        body: SingleChildScrollView(
+          child: Stack(
+            children: <Widget>[
+              Container(
+                width: double.maxFinite,
+                height: height / 9,
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                      begin: Alignment.topLeft,
+                      end: Alignment.bottomRight,
+                      colors: <Color>[Colors.blue[200], Colors.blue]),
+                  borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(80.0),
+                      bottomRight: Radius.circular(80.0)),
+                ),
+              ),
+              Container(
+                child: Column(
+                  mainAxisSize: MainAxisSize.max,
+                  children: <Widget>[
+                    SizedBox(
+                      height: height / 48,
+                    ),
+                    Center(
+                        child: Row(
+                      mainAxisSize: MainAxisSize.max,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        ChoiceChip(
+                          label: Text('Sinhala'),
+                          elevation: 5,
+                          pressElevation: 10.0,
+                          selected: isSelected,
+                          selectedColor: Colors.green,
+                          onSelected: (bool value) {
+                            setState(() {
+                              isSelected = true;
+                            });
+                          },
+                          backgroundColor: Colors.blue,
+                          labelStyle: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: Colors.grey[100]),
+                        ),
+                        SizedBox(
+                          width: 3,
+                        ),
+                        ChoiceChip(
+                          label: Text(
+                            'English',
+                          ),
+                          selected: !isSelected,
+                          elevation: 5,
+                          pressElevation: 10.0,
+                          selectedColor: Colors.green,
+                          onSelected: (bool value) {
+                            setState(() {
+                              isSelected = false;
+                            });
+                          },
+                          backgroundColor: Colors.blue,
+                          labelStyle: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: Colors.grey[100]),
+                        ),
+                      ],
+                    )),
+                  ],
+                ),
+              ),
+              getContent(height),
+            ],
+          ),
+        ));
+  }
+
+  Widget getContent(height) {
+    double containerHeight = height / 9;
+    return Padding(
+      padding: const EdgeInsets.only(left: 8.0, right: 8.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          SizedBox(
+            height: containerHeight - 4,
+          ),
+          getTopicChip('Recent'),
+          Container(
+              //color: Colors.white,
+              height: height / 3,
+              width: double.maxFinite,
+              child: ListView(
+                shrinkWrap: true,
+                scrollDirection: Axis.horizontal,
+                children: <Widget>[
+                  getRecentCardComponent(height),
+                  getRecentCardComponent(height),
+                  getRecentCardComponent(height),
+                  getRecentCardComponent(height),
+                  getRecentCardComponent(height)
+                ],
+              )),
+          getTopicChip('Tutorials'),
+          Container(
+              //color: Colors.white,
+              height: height / 6,
+              width: double.maxFinite,
+              child: ListView(
+                shrinkWrap: true,
+                scrollDirection: Axis.horizontal,
+                children: <Widget>[
+                  getCardComponent(height),
+                  getCardComponent(height),
+                  getCardComponent(height),
+                  getCardComponent(height),
+                  getCardComponent(height)
+                ],
+              )),
+          getTopicChip('Other'),
+          Container(
+              //color: Colors.white,
+              height: height / 6,
+              width: double.maxFinite,
+              child: ListView(
+                shrinkWrap: true,
+                scrollDirection: Axis.horizontal,
+                children: <Widget>[
+                  getCardComponent(height),
+                  getCardComponent(height),
+                  getCardComponent(height),
+                  getCardComponent(height),
+                  getCardComponent(height)
+                ],
+              )),
+        ],
       ),
-      drawer: buildDrawer(),
-      body: Center(),
     );
   }
 }
